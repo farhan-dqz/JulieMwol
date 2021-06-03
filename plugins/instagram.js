ac81323c0385be216bf3745e1160caa4b04b7a43
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-06-03 21:51:19 +0530

    Update instagram.js

--- plugins/instagram.js
+++ plugins/instagram.js
@@ -1,19 +1,5 @@
 /*
-# Copyright (C) 2020 MuhammedKpln.
-#           edited and brought into action by Vai838
-# WhatsAsena is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# WhatsAsena is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
+# Copyright (C) 2020 farhan-dqz
 */
 
 
@@ -29,7 +15,7 @@ const Lang = Language.getString('instagram') ;
 
 
 
-Asena.addCommand({ pattern: 'profinsta ?(.*)', fromMe: false, usage: Lang.USAGE, desc: Lang.DESC }, async (message, match) => {
+Asena.addCommand({ pattern: 'profinsta ?(.*)', fromMe: true, usage: Lang.USAGE, desc: Lang.DESC }, async (message, match) => {
 
     const userName = match[1]
 
