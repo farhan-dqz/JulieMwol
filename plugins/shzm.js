6b618920785e87483a8543ec5571c53a9c9495cc
afnanplk <83832372+afnanplk@users.noreply.github.com>
2021-06-04 00:01:40 +0530

    Update shzm.js

--- plugins/shzm.js
+++ plugins/shzm.js
@@ -7,7 +7,7 @@ const Asena = require('../events');
 const { MessageType } = require('@adiwajshing/baileys');
 const axios = require('axios');
 
-Asena.addCommand({pattern: 'shazam', fromMe: true, desc: 'replay to a audio to find th song' }, (async (message, match) => { 
+Asena.addCommand({pattern: 'find', fromMe: true, desc: 'replay to a audio to find th song' }, (async (message, match) => { 
 
     if (message.reply_message === false) return await message.client.sendMessage(message.jid, bix.UV_REPLY, MessageType.text);
 
