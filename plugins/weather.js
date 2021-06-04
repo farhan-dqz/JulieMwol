204ce55a7a0cd90ed8d5f0881c728d2e17e6d137
afnanplk <83832372+afnanplk@users.noreply.github.com>
2021-06-04 14:03:40 +0530

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -34,7 +34,33 @@ if (Config.WORKTYPE == 'private') {
 		    return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text);
 	    }
     });
-}
+Asena.addCommand({ pattern: 'movie ?(.*)', fromMe: false, desc: "Shows movie info." }, (async (message, match) => {
+	if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a name.```', MessageType.text, { quoted: message.data });
+	let url = `http://www.omdbapi.com/?apikey=742b2d09&t=${match[1]}&plot=full`
+	const response = await got(url);
+	const json = JSON.parse(response.body);
+	if (json.Response != 'True') return await message.client.sendMessage(message.jid, '*Not found  please check the spelling.*', MessageType.text, { quoted: message.data });
+	let msg = '```';
+	msg += 'Title      : ' + json.Title + '\n\n';
+	msg += 'Year       : ' + json.Year + '\n\n';
+	msg += 'Rated      : ' + json.Rated + '\n\n';
+	msg += 'Released   : ' + json.Released + '\n\n';
+	msg += 'Runtime    : ' + json.Runtime + '\n\n';
+	msg += 'Genre      : ' + json.Genre + '\n\n';
+	msg += 'Director   : ' + json.Director + '\n\n';
+	msg += 'Writer     : ' + json.Writer + '\n\n';
+	msg += 'Actors     : ' + json.Actors + '\n\n';
+	msg += 'Plot       : ' + json.Plot + '\n\n';
+	msg += 'Language   : ' + json.Language + '\n\n';
+	msg += 'Country    : ' + json.Country + '\n\n';
+	msg += 'Awards     : ' + json.Awards + '\n\n';
+	msg += 'BoxOffice  : ' + json.BoxOffice + '\n\n';
+	msg += 'Production : ' + json.Production + '\n\n';
+	msg += 'imdbRating : ' + json.imdbRating + '\n\n';
+	msg += 'imdbVotes  : ' + json.imdbVotes + '```';
+	await message.client.sendMessage(message.jid, msg, MessageType.text, { quoted: message.data });
+ }));
+}	
 if (Config.WORKTYPE == 'public') {
 
     Asena.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: false}, async (message, match) => {
@@ -71,4 +97,30 @@ if (Config.WORKTYPE == 'public') {
 		    return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text);
 	    }
     });
+Asena.addCommand({ pattern: 'movie ?(.*)', fromMe: false, desc: "Shows movie info." }, (async (message, match) => {
+	if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a name.```', MessageType.text, { quoted: message.data });
+	let url = `http://www.omdbapi.com/?apikey=742b2d09&t=${match[1]}&plot=full`
+	const response = await got(url);
+	const json = JSON.parse(response.body);
+	if (json.Response != 'True') return await message.client.sendMessage(message.jid, '*Not found  please check the spelling.*', MessageType.text, { quoted: message.data });
+	let msg = '```';
+	msg += 'Title      : ' + json.Title + '\n\n';
+	msg += 'Year       : ' + json.Year + '\n\n';
+	msg += 'Rated      : ' + json.Rated + '\n\n';
+	msg += 'Released   : ' + json.Released + '\n\n';
+	msg += 'Runtime    : ' + json.Runtime + '\n\n';
+	msg += 'Genre      : ' + json.Genre + '\n\n';
+	msg += 'Director   : ' + json.Director + '\n\n';
+	msg += 'Writer     : ' + json.Writer + '\n\n';
+	msg += 'Actors     : ' + json.Actors + '\n\n';
+	msg += 'Plot       : ' + json.Plot + '\n\n';
+	msg += 'Language   : ' + json.Language + '\n\n';
+	msg += 'Country    : ' + json.Country + '\n\n';
+	msg += 'Awards     : ' + json.Awards + '\n\n';
+	msg += 'BoxOffice  : ' + json.BoxOffice + '\n\n';
+	msg += 'Production : ' + json.Production + '\n\n';
+	msg += 'imdbRating : ' + json.imdbRating + '\n\n';
+	msg += 'imdbVotes  : ' + json.imdbVotes + '```';
+	await message.client.sendMessage(message.jid, msg, MessageType.text, { quoted: message.data });
+ }));
 }
