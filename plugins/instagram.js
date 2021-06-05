4ecc65f526cbe1ae327214dcb6c0d8a4e31bfa5d
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-06-05 10:37:01 +0530

    Update instagram.js

--- plugins/instagram.js
+++ plugins/instagram.js
@@ -13,7 +13,7 @@ const Lang = Language.getString('instagram') ;
 
 
 
-
+if (Config.WORKTYPE == 'private') {
 
 Asena.addCommand({ pattern: 'profinsta ?(.*)', fromMe: true, usage: Lang.USAGE, desc: Lang.DESC }, async (message, match) => {
 
@@ -52,5 +52,41 @@ Asena.addCommand({ pattern: 'profinsta ?(.*)', fromMe: true, usage: Lang.USAGE, 
   },
 
  )
+else if (Config.WORKTYPE == 'private') {
 
+Asena.addCommand({ pattern: 'profinsta ?(.*)', fromMe: false, usage: Lang.USAGE, desc: Lang.DESC }, async (message, match) => {
+
+    const userName = match[1]
+
+    if (!userName) return await message.sendMessage(errorMessage(Lang.NEED_WORD))
+
+    await message.sendMessage(infoMessage(Lang.LOADING))
+
+    await axios
+      .get(`https://api-anoncybfakeplayer.herokuapp.com/igstalk?username=${userName}`)
+      .then(async (response) => {
+        const {
+          pic,
+          username,
+          bio,
+          follower,
+          following,
+        } = response.data
+
+        const profileBuffer = await axios.get(pic, {responseType: 'arraybuffer'})
+
+        const msg = `
+        *${Lang.USERNAME}*: ${username}    
+        *${Lang.BIO}*: ${bio}
+        *${Lang.FOLLOWERS}*: ${follower}
+        *${Lang.FOLLOWS}*: ${following}`
+
+        await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
+          caption: msg
+        })
+      })
+      .catch(
+        async (err) => await message.sendMessage(errorMessage(Lang.NOT_FOUND + userName)),
+      )
+  },
 
