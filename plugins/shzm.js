65d27ee5783990298264a965c140841e34a3ffa9
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-06-05 10:28:41 +0530

    Update shzm.js

--- plugins/shzm.js
+++ plugins/shzm.js
@@ -7,6 +7,39 @@ const Asena = require('../events');
 const { MessageType } = require('@adiwajshing/baileys');
 const axios = require('axios');
 
+
+if (Config.WORKTYPE == 'private') {
+
+
+Asena.addCommand({pattern: 'find', fromMe: true, desc: 'replay to a audio to find th song' }, (async (message, match) => { 
+
+    if (message.reply_message === false) return await message.client.sendMessage(message.jid, bix.UV_REPLY, MessageType.text);
+
+    var location = await message.client.downloadAndSaveMediaMessage({
+        key: {
+            remoteJid: message.reply_message.jid,
+            id: message.reply_message.id
+        },
+        message: message.reply_message.data.quotedMessage
+    });
+
+    ffmpeg(location)
+        .format('mp3')
+        .save('lyr.mp3')
+        .on('end', async () => {
+
+            var data = { 'api_token': '6a03ebccfc18bb19e3fb7bb5280a51ab', 'file': fs.createReadStream('./lyr.mp3'), 'return': 'apple_music,spotify' };
+            req ({ uri: 'https://api.audd.io/', form: data, method: "POST" }, async (err, res, body) => {
+                return await message.client.sendMessage(message.jid, body, MessageType.text);
+            })
+        })
+    }));
+}
+
+
+if (Config.WORKTYPE == 'public') {
+
+
 Asena.addCommand({pattern: 'find', fromMe: true, desc: 'replay to a audio to find th song' }, (async (message, match) => { 
 
     if (message.reply_message === false) return await message.client.sendMessage(message.jid, bix.UV_REPLY, MessageType.text);
