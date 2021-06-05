5d75c9e1e58b598a3ddf8d92e82f4fdf9940fa4f
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-06-05 10:46:10 +0530

    Update simsimh.js

--- plugins/simsimh.js
+++ plugins/simsimh.js
@@ -11,7 +11,9 @@ const got = require('got');
 const Language = require('../language');
 const Lang = Language.getString('weather');
 
-Asena.addCommand({pattern: 'molu ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
+if (Config.WORKTYPE == 'private') {
+
+    Asena.addCommand({pattern: 'molu ?(.*)', fromMe: true, desc: Lang.BOT_DESC}, async (message, match) => {
 	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
 	try {
@@ -21,4 +23,19 @@ Asena.addCommand({pattern: 'molu ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, as
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
 	}
-});
+    });
+}
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({pattern: 'molu ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
+	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
+	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
+	try {
+		const response = await got(url);
+		const json = JSON.parse(response.body);
+	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n```moluse🐦``` ' + Lang.BOT +' ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
+	} catch {
+		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
+	}
+    });
+}
