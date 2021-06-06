e93d58dceb9ec26e8ce715257e885eace3f2ff89
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-06-06 19:08:29 +0530

    Update conventer.js

--- plugins/conventer.js
+++ plugins/conventer.js
@@ -122,7 +122,7 @@ else if (Config.WORKTYPE == 'public') {
         return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }));
 
-    Asena.addCommand({pattern: 'imagesticker', fromMe: true, desc: Lang.STİCKER_DESC, dontAddCommandList: true}, (async (message, match) => {   
+    Asena.addCommand({pattern: 'photo', fromMe: true, desc: Lang.STİCKER_DESC, dontAddCommandList: true}, (async (message, match) => {   
  
         if (message.reply_message === false) return await message.client.sendMessage(message.jid, Lang.STİCKER_NEEDREPLY, MessageType.text);
         var downloading = await message.client.sendMessage(message.jid,Lang.STİCKER,MessageType.text);
