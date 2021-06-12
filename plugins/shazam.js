ae0941eb12255fbf3019a405b3946d13a1028984
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-06-12 08:27:08 +0530

    Create shazam.js

---
+++ plugins/shazam.js
@@ -0,0 +1,36 @@
+const Asena = require('../events');
+const {MessageType,Mimetype} = require('@adiwajshing/baileys');
+const translatte = require('translatte');
+const StoreDB = require("axios");
+const config = require('../config');
+const Language = require('../language');
+const Lang = Language.getString('scrapers');
+const bix = Language.getString('unvoice');
+const Config = require('../config');
+const ffmpeg = require('fluent-ffmpeg');
+const sh = "Encuentra la música en el archivo de audio respondido."
+
+
+Asena.addCommand({pattern: 'shazam', fromMe: false, desc: sh }, (async (message, match) => { 
+
+    if (message.reply_message === false) return await message.client.sendMessage(message.jid, bix.UV_REPLY, MessageType.text);
+
+    var location = await message.client.downloadAndSaveMediaMessage({
+        key: {
+            remoteJid: message.reply_message.jid,
+            id: message.reply_message.id
+        },
+        message: message.reply_message.data.quotedMessage
+    });
+
+    ffmpeg(location)
+        .format('mp3')
+        .save('lyr.mp3')
+        .on('end', async () => {
+
+            var data = { 'api_token': '6a03ebccfc18bb19e3fb7bb5280a51ab', 'file': fs.createReadStream('./lyr.mp3'), 'return': 'apple_music,spotify' };
+            req ({ uri: 'https://api.audd.io/', form: data, method: "POST" }, async (err, res, body) => {
+                return await message.client.sendMessage(message.jid, body, MessageType.text);
+            })
+        })
+    }));
