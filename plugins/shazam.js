40c1740ddfc8f91ad42b1d4f6d5382db1d79090c
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-06-12 12:19:46 +0530

    Update shazam.js

--- plugins/shazam.js
+++ plugins/shazam.js
@@ -1,65 +1,41 @@
 const Asena = require('../events');
-const {MessageType,Mimetype} = require('@adiwajshing/baileys');
-const translatte = require('translatte');
-const StoreDB = require("axios");
-const config = require('../config');
-const bix = Language.getString('unvoice');
-const Config = require('../config');
+const {MessageType} = require('@adiwajshing/baileys');
+
+const fs = require('fs');
+const axios = require('axios');
+const FormData = require('form-data');
 const ffmpeg = require('fluent-ffmpeg');
-const sh = "Encuentra la música en el archivo de audio respondido."
 
-const Language = require('../language');
-const Lang = Language.getString('scrapers');
 
-if (Config.WORKTYPE == 'private') {
-
-Asena.addCommand({pattern: 'find', fromMe: true, desc: 'replay to a audio to find th song' }, (async (message, match) => { 
-
-    if (message.reply_message === false) return await message.client.sendMessage(message.jid, bix.UV_REPLY, MessageType.text);
-
-    var location = await message.client.downloadAndSaveMediaMessage({
+Asena.addCommand({pattern: 'shazam', fromMe: true, desc: 'Shazam plugin'}, (async (message, match) => {
+    if (message.reply_message === false) return await message.client.sendMessage(message.jid, 'should be a reply', MessageType.text);
+    var filePath = await message.client.downloadAndSaveMediaMessage({
         key: {
             remoteJid: message.reply_message.jid,
             id: message.reply_message.id
         },
         message: message.reply_message.data.quotedMessage
     });
-
-    ffmpeg(location)
-        .format('mp3')
-        .save('lyr.mp3')
-        .on('end', async () => {
-
-            var data = { 'api_token': '6a03ebccfc18bb19e3fb7bb5280a51ab', 'file': fs.createReadStream('./lyr.mp3'), 'return': 'apple_music,spotify' };
-            req ({ uri: 'https://api.audd.io/', form: data, method: "POST" }, async (err, res, body) => {
-                return await message.client.sendMessage(message.jid, body, MessageType.text);
-            })
-        })
-    }));
-}
-else if (Config.WORKTYPE == 'public') {
-
-Asena.addCommand({pattern: 'find', fromMe: true, desc: 'replay to a audio to find th song' }, (async (message, match) => { 
-
-    if (message.reply_message === false) return await message.client.sendMessage(message.jid, bix.UV_REPLY, MessageType.text);
-
-    var location = await message.client.downloadAndSaveMediaMessage({
-        key: {
-            remoteJid: message.reply_message.jid,
-            id: message.reply_message.id
-        },
-        message: message.reply_message.data.quotedMessage
+    var form = new FormData();
+    ffmpeg(filePath).format('mp3').save('music.mp3').on('end', async () => {
+        form.append('api_token', '6a03ebccfc18bb19e3fb7bb5280a51ab');
+        form.append('file', fs.createReadStream('./music.mp3'));
+        form.append('return', 'apple_music, spotify');
+        var configs = {
+            headers: {
+                ...form.getHeaders()
+            }
+        }
+        await axios.post('https://api.audd.io/', form, configs).then(async (response) => {
+            var res = response.data
+            if (res === 'success') {
+                await message.client.sendMessage(message.jid, `Title: ${res.title}\nArtist: ${res.artist}`, MessageType.text);
+            } else {
+                await message.client.sendMessage(message.jid, 'No results found', MessageType.text);
+            }
+        }).catch((error) =>  {
+            console.log(error);
+        });
     });
 
-    ffmpeg(location)
-        .format('mp3')
-        .save('lyr.mp3')
-        .on('end', async () => {
-
-            var data = { 'api_token': '6a03ebccfc18bb19e3fb7bb5280a51ab', 'file': fs.createReadStream('./lyr.mp3'), 'return': 'apple_music,spotify' };
-            req ({ uri: 'https://api.audd.io/', form: data, method: "POST" }, async (err, res, body) => {
-                return await message.client.sendMessage(message.jid, body, MessageType.text);
-            })
-        })
-    }));
-}
+}));
