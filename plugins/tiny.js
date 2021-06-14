f4af669f5740c8a1d6f76d7c6f3f48fc5c754994
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-06-14 15:10:22 +0530

    Update tiny.js

--- plugins/tiny.js
+++ plugins/tiny.js
@@ -1,6 +1,7 @@
 const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const got = require('got');
+const Config = require('../config');
 
 const Language = require('../language');
 const Lang = Language.getString('weather');
