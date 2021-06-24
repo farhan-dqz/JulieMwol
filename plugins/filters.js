324c60f85dd7f815eda79e4a806245b1053f4ab4
Ameer Suhail <86001869+ameerkallumthodi@users.noreply.github.com>
2021-06-24 23:46:21 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -56,7 +56,7 @@ const array = ['name entha','Helo','King','Kooi','Tuttu','Azaru','Ramos','Tentac
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
-       await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/' + a + '.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: true})
+       await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/' + a + '.mp3' + '.mpeg' '.mp4'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: true})
 }
 });
 
