5aa6663fed0a0d4d2f7b88f438c577c4f3e5815d
saidalisaid2 <86299543+saidalisaid2@users.noreply.github.com>
2021-07-04 19:19:50 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -802,7 +802,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
                                     '\n*JulieMwol an error has occurred!*'+
                                     '\n*This error log may include your number or the number of an opponent. Please be careful with it!*' +
                                     '\n*You can write to our Telegram group for help.*' +
-                                    '\n*Aslo you can join our support group:* https://chat.whatsapp.com/IlmB0J0SilOIJRq1CLotXT' +
+                                    '\n*Aslo you can join our support group:* https://chat.whatsapp.com/KaxvN2d3ws117gyCtMCU2D' +
                                     '\n*This message should have gone to your number (saved messages).*\n\n' +
                                     '*Error:* ```' + error + '```\n\n'
                                     , MessageType.text, {detectLinks: false}
