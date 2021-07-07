a327f95846a3b0d3b8bd100c76dfeb058e94a5f7
Sunaifnfc <87078360+Sunaifnfc@users.noreply.github.com>
2021-07-07 20:55:02 +0530

    Update joke.js

--- plugins/joke.js
+++ plugins/joke.js
@@ -12,7 +12,7 @@ Asena.addCommand({pattern: 'joke ?(.*)', fromMe: false, desc: Lang.JOKE_DESC}, a
 		const response = await got(url);
 		const json = JSON.parse(response.body);
 		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*🗣️ ' + Lang.JOKE +'* ```' + json.setup + '```\n\n' +
-		'*😆' + Lang.PUNCHLINE +'* ```' + json.punchline+ '```\n', MessageType.text);
+		'*😝' + Lang.PUNCHLINE +'* ```' + json.punchline+ '```\n', MessageType.text);
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
 	}
