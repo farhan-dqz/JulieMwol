b2bdc73d71ac9a0bf5fda5020e7f49c891f5d376
Sunaifnfc <87078360+Sunaifnfc@users.noreply.github.com>
2021-07-07 20:59:27 +0530

    Update movie.js

--- plugins/movie.js
+++ plugins/movie.js
@@ -2,7 +2,7 @@ const Asena = require('../events');
 const { MessageType } = require('@adiwajshing/baileys');
 const got = require('got');
 //LyFE
-Asena.addCommand({ pattern: 'movie ?(.*)', fromMe: false, desc: "Shows movie info." }, (async (message, match) => {
+Asena.addCommand({ pattern: 'movie ?(.*)', fromMe: false, desc: "Shows movies info." }, (async (message, match) => {
 	if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a name.```', MessageType.text, { quoted: message.data });
 	let url = `http://www.omdbapi.com/?apikey=742b2d09&t=${match[1]}&plot=full`
 	const response = await got(url);
