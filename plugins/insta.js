b8155eef218f9443072831b7642bf1c9ab479c88
Sunaifnfc <87078360+Sunaifnfc@users.noreply.github.com>
2021-07-07 20:53:03 +0530

    Update insta.js

--- plugins/insta.js
+++ plugins/insta.js
@@ -25,10 +25,10 @@ Asena.addCommand({ pattern: 'vinsta ?(.*)', fromMe: false, desc: sd }, async (me
         const profileBuffer = await axios.get(resource.url[0], { responseType: 'arraybuffer' })
 
         if (resource.url.is_video[0]) {
-            await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, { caption: 'Made by WhatsAsena' })
+            await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, { caption: 'Made by Sunaif Adkar 🏌️' })
         }
         else if (!resource.url.is_video[0]) {
-            await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, { caption: 'Made by WhatsAsena' })
+            await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, { caption: 'Made by Sunaif Adkar 🏌️' })
         }
     }).catch(async (err) => {
         await message.sendMessage(errorMessage(Lang.NOT_FOUND + userName))
