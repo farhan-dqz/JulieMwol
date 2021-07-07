b1b4e35fcc3e453db7ea22433454c29cb5371b0a
Sunaifnfc <87078360+Sunaifnfc@users.noreply.github.com>
2021-07-07 20:01:28 +0530

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -97,7 +97,7 @@ if (Config.WORKTYPE == 'private') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by WhatsAsenaPublic'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by Sunaif Adkar 🏌️Public'})
 
     }));
 }
@@ -191,7 +191,7 @@ else if (Config.WORKTYPE == 'public') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by WhatsAsenaPublic'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by Sunaif Adkar 🏌️Public'})
 
     }));
 }
