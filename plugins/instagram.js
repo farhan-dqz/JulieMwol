2ab03081a21f115eae435d091b6713efbee0774c
fasilvkn <84661141+fasilvkn@users.noreply.github.com>
2021-07-12 18:27:05 +0530

    Update instagram.js

--- plugins/instagram.js
+++ plugins/instagram.js
@@ -1,96 +1,131 @@
 /*
-# Copyright (C) 2020 farhan-dqz
+# Copyright (C) 2020 MuhammedKpln.
+# edited by Vai838
+# WhatsAsena is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# WhatsAsena is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 */
 
+const Asena = require('../events')
+const { MessageType } = require('@adiwajshing/baileys')
+const axios = require('axios')
+const cn = require('../config');
 
-const Asena = require('../events');
-const { MessageType } = require('@adiwajshing/baileys');
-const axios = require('axios');
-const Config = require('../config');
-
-const Language = require('../language');
-const { errorMessage, infoMessage } = require('../helpers');
-const Lang = Language.getString('instagram') ;
-
+const Language = require('../language')
+const { errorMessage, infoMessage } = require('../helpers')
+const Lang = Language.getString('instagram')
 
 
-if (Config.WORKTYPE == 'private') {
+if (cn.WORKTYPE == 'private') {
 
-Asena.addCommand({ pattern: 'profinsta ?(.*)', fromMe: true, usage: Lang.USAGE, desc: Lang.DESC }, async (message, match) => {
+    Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: true, usage: Lang.USAGE, desc: Lang.DESC }, async (message, match) => {
 
-    const userName = match[1]
+        if (message.jid === '905524317852-1612300121@g.us') {
 
-    if (!userName) return await message.sendMessage(errorMessage(Lang.NEED_WORD))
+            return;
+        }
 
-    await message.sendMessage(infoMessage(Lang.LOADING))
 
-    await axios
-      .get(`https://api-anoncybfakeplayer.herokuapp.com/igstalk?username=${userName}`)
-      .then(async (response) => {
-        const {
-          pic,
-          username,
-          bio,
-          follower,
-          following,
-        } = response.data
+        const userName = match[1]
 
-        const profileBuffer = await axios.get(pic, {responseType: 'arraybuffer'})
+        if (!userName) return await message.sendMessage(errorMessage(Lang.NEED_WORD))
 
-        const msg = `
-        *${Lang.USERNAME}*: ${username}    
-        *${Lang.BIO}*: ${bio}
-        *${Lang.FOLLOWERS}*: ${follower}
-        *${Lang.FOLLOWS}*: ${following}`
+        await message.sendMessage(infoMessage(Lang.LOADING))
 
-        await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
-          caption: msg
-        })
-      })
-      .catch(
-        async (err) => await message.sendMessage(errorMessage(Lang.NOT_FOUND + userName)),
-      )
-  },
+        await axios
+          .get(`https://videfikri.com/api/igstalk/?username=${userName}`)
+          .then(async (response) => {
+            const {
+              profile_hd,
+              username,
+              bio,
+              followers,
+              following,
+              full_name,
+              is_private,
+            } = response.data.result
 
- )
+            const profileBuffer = await axios.get(profile_hd, {
+              responseType: 'arraybuffer',
+            })
+
+            const msg = `
+            *${Lang.NAME}*: ${full_name}
+            *${Lang.USERNAME}*: ${username}
+            *${Lang.BIO}*: ${bio}
+            *${Lang.FOLLOWERS}*: ${followers}
+            *${Lang.FOLLOWS}*: ${following}
+            *${Lang.ACCOUNT}*: ${is_private ? Lang.HIDDEN : Lang.PUBLIC}`
+
+            await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
+              caption: msg,
+            })
+          })
+          .catch(
+            async (err) => await message.sendMessage(errorMessage(Lang.NOT_FOUND + userName)),
+          )
+      },
+    )
 }
-else if (Config.WORKTYPE == 'public') {
+else if (cn.WORKTYPE == 'public') {
 
-Asena.addCommand({ pattern: 'profinsta ?(.*)', fromMe: false, usage: Lang.USAGE, desc: Lang.DESC }, async (message, match) => {
+    Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, usage: Lang.USAGE, desc: Lang.DESC }, async (message, match) => {
 
-    const userName = match[1]
+        if (message.jid === '905524317852-1612300121@g.us') {
 
-    if (!userName) return await message.sendMessage(errorMessage(Lang.NEED_WORD))
+            return;
+        }
 
-    await message.sendMessage(infoMessage(Lang.LOADING))
 
-    await axios
-      .get(`https://api-anoncybfakeplayer.herokuapp.com/igstalk?username=${userName}`)
-      .then(async (response) => {
-        const {
-          pic,
-          username,
-          bio,
-          follower,
-          following,
-        } = response.data
+        const userName = match[1]
 
-        const profileBuffer = await axios.get(pic, {responseType: 'arraybuffer'})
+        if (!userName) return await message.sendMessage(errorMessage(Lang.NEED_WORD))
 
-        const msg = `
-        *${Lang.USERNAME}*: ${username}    
-        *${Lang.BIO}*: ${bio}
-        *${Lang.FOLLOWERS}*: ${follower}
-        *${Lang.FOLLOWS}*: ${following}`
+        await message.sendMessage(infoMessage(Lang.LOADING))
 
-        await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
-          caption: msg
-        })
-      })
-      .catch(
-        async (err) => await message.sendMessage(errorMessage(Lang.NOT_FOUND + userName)),
-      )
-  },
+        await axios
+          .get(`https://videfikri.com/api/igstalk/?username=${userName}`)
+          .then(async (response) => {
+            const {
+              profile_hd,
+              username,
+              bio,
+              followers,
+              following,
+              full_name,
+              is_private,
+            } = response.data.result
 
- )
+            const profileBuffer = await axios.get(profile_hd, {
+              responseType: 'arraybuffer',
+            })
+
+            const msg = `
+            *${Lang.NAME}*: ${full_name}
+            *${Lang.USERNAME}*: ${username}
+            *${Lang.BIO}*: ${bio}
+            *${Lang.FOLLOWERS}*: ${followers}
+            *${Lang.FOLLOWS}*: ${following}
+            *${Lang.ACCOUNT}*: ${is_private ? Lang.HIDDEN : Lang.PUBLIC}`
+
+            await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
+              caption: msg,
+            })
+          })
+          .catch(
+            async (err) => await message.sendMessage(errorMessage(Lang.NOT_FOUND + userName)),
+          )
+      },
+    )
 }
+
