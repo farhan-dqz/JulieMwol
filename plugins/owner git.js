9fb8c592411163ce073e015edead986008e0edbf
Ameer Suhail <86001869+ameer-kallumthodi@users.noreply.github.com>
2021-07-14 17:29:57 +0530

    Create owner git.js

---
+++ plugins/owner git.js
@@ -0,0 +1,64 @@
+const asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const OWNER = "it sends details of owner"
+const GIT = "it sends links"
+const Config = require('../config');
+
+
+if (Config.WORKTYPE == 'private') {
+asena.addCommand({pattern: 'owner', fromMe: true, deleteCommand: true, desc: OWNER,}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    r_text[1] = "*╔═════◉Pikachu(Ameer)◉═════╗*\n           Pikachu🪐\n*❖═List═❖*\n\n*💠owner - http://Wa.me/+919946432377*\n*💠https://www.instagram.com/ameer_.su_hail?r=nametag            *\n*╚═══❖══▣══▣══❖═══╝*\n\n*▷Creator: Ameer💫*"
+
+    
+    await message.client.sendMessage(
+        message.jid,(r_text[1]), MessageType.text);
+
+    }));
+
+
+asena.addCommand({pattern: 'git', fromMe: true, deleteCommand: true, desc: GIT,}, (async (message, match) => {
+
+        var r_text = new Array ();
+    
+        r_text[1] = "*╔═════🔅Git links🔅═════╗*\n           Pikachu\n*❖═Owner Ameer Suhail═❖*\n\n*�⚜https://github.com/ameer-kallumthodi/pikachu*\n*     Liyamol\n*❖═Owner Saidali═❖*\n\n*�⚜https://github.com/saidalisaid2/LiyaMol        \n*╚═══❖══▣══▣══❖═══╝*\n\n*▷Creator: Ameer*"
+
+    
+        await message.client.sendMessage(
+            message.jid,(r_text[i]), MessageType.text);
+    
+        }));    
+
+    }
+    
+
+    if (Config.WORKTYPE == 'public') {
+        asena.addCommand({pattern: 'owner', fromMe: true, deleteCommand: true, desc: OWNER,}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    r_text[1] = "*╔═════◉Pikachu(Ameer)◉═════╗*\n           Pikachu🪐\n*❖═List═❖*\n\n*💠owner - http://Wa.me/+919946432377*\n*💠https://www.instagram.com/ameer_.su_hail?r=nametag            *\n*╚═══❖══▣══▣══❖═══╝*\n\n*▷Creator: Ameer💫*"
+
+    
+    await message.client.sendMessage(
+        message.jid,(r_text[1]), MessageType.text);
+
+    }));
+
+
+asena.addCommand({pattern: 'git', fromMe: true, deleteCommand: true, desc: GIT,}, (async (message, match) => {
+
+        var r_text = new Array ();
+    
+        r_text[1] = "*╔═════🔅Git links🔅═════╗*\n           Pikachu\n*❖═Owner Ameer Suhail═❖*\n\n*💠https://github.com/ameer-kallumthodi/pikachu*\n*     Liyamol\n*❖═Owner Saidali═❖*\n\n*💠https://github.com/saidalisaid2/LiyaMol        \n*╚═══❖══▣══▣══❖═══╝*\n\n*▷Creator: Ameer*"
+
+    
+        await message.client.sendMessage(
+            message.jid,(r_text[i]), MessageType.text);
+    
+        }));    
+
+    }
+    
