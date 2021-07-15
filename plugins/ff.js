8612cb7449157eda5fa1d9de7e9d5dcbe16115cd
fasilvkn <84661141+fasilvkn@users.noreply.github.com>
2021-07-15 10:50:54 +0530

    Update ff.js

--- plugins/ff.js
+++ plugins/ff.js
@@ -13,7 +13,7 @@ const need = "*type some word after command*\n*command enthelum perupara nenba"
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({ pattern: 'rff ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'ff ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(need);
 
@@ -26,7 +26,7 @@ if (Config.WORKTYPE == 'private') {
 
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'rff ?(.*)', fromMe: false,dontAddCommandList: true }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'ff ?(.*)', fromMe: false,dontAddCommandList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(need);
 
