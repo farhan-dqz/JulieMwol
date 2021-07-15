0122ab8978e64703e882cac9da2e532329de702b
Ameer Suhail <86001869+ameer-kallumthodi@users.noreply.github.com>
2021-07-15 21:00:29 +0530

    Update search.js

--- plugins/search.js
+++ plugins/search.js
@@ -4,23 +4,9 @@ const Config = require('../config');
 const axios = require('axios');
 const Sr = "Search on Google."
 
-if (CON.WORKTYPE == 'private') {
-
 Asena.addCommand({pattern: 'search ?(.*)', fromMe: true, desc: Sr}, (async (message, match) => {
 
     if (match[1] === '') return await message.client.sendMessage(message.jid,'```entha monoose vendath```', MessageType.text);
- 
-    Sea.google(`${match[1]}`).then(async(result) => {
-        
-        await message.client.sendMessage(message.jid,result[0], MessageType.text);
-      });
-}));
-}
-
-else if (CON.WORKTYPE == 'public') {
-
-    Asena.addCommand({pattern: 'search ?(.*)', fromMe: false, desc: Sr}, (async (message, match) => {
-        if (match[1] === '') return await message.client.sendMessage(message.jid,'```entha monoose vendath```', MessageType.text);
  
     Sea.google(`${match[1]}`).then(async(result) => {
         
