c591f1f0b01c27585f94263643fbac3c32f6d8e7
Ameer Suhail <86001869+ameer-kallumthodi@users.noreply.github.com>
2021-07-15 19:47:59 +0530

    Update search.js

--- plugins/search.js
+++ plugins/search.js
@@ -4,7 +4,9 @@ const Config = require('../config');
 const axios = require('axios');
 const Sr = "Search on Google."
 
-Asena.addCommand({pattern: 'search ?(.*)', fromMe: false, desc: Sr}, (async (message, match) => {
+if (CON.WORKTYPE == 'private') {
+
+Asena.addCommand({pattern: 'search ?(.*)', fromMe: true, desc: Sr}, (async (message, match) => {
 
     if (match[1] === '') return await message.client.sendMessage(message.jid,'```entha monoose vendath```', MessageType.text);
  
@@ -13,3 +15,16 @@ Asena.addCommand({pattern: 'search ?(.*)', fromMe: false, desc: Sr}, (async (mes
         await message.client.sendMessage(message.jid,result[0], MessageType.text);
       });
 }));
+}
+
+else if (CON.WORKTYPE == 'public') {
+
+    Asena.addCommand({pattern: 'search ?(.*)', fromMe: false, desc: Sr}, (async (message, match) => {
+        if (match[1] === '') return await message.client.sendMessage(message.jid,'```entha monoose vendath```', MessageType.text);
+ 
+    Sea.google(`${match[1]}`).then(async(result) => {
+        
+        await message.client.sendMessage(message.jid,result[0], MessageType.text);
+      });
+}));
+}
