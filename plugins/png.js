462fbc5e037d955b4ab9be6c7d40ea3393ae5936
fasilvkn <84661141+fasilvkn@users.noreply.github.com>
2021-07-17 10:30:53 +0530

    Update png.js

--- plugins/png.js
+++ plugins/png.js
@@ -46,7 +46,7 @@ else if (Config.WORKTYPE == 'public') {
 
         var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/emoji2png?emoji=${encodeURIComponent(match[1])}&type=apple`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Coded By Ravindu Manoj'})
+        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Coded By Paathu'})
 
     }));
 
