1bc33cb93b7d9d0765a0e00e7607625f90eb7f16
fasilvkn <84661141+fasilvkn@users.noreply.github.com>
2021-07-19 10:11:17 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -137,24 +137,9 @@ if(pattern.test(message.message)){
             if (pattern.test(message.message)) {
                 await message.client.sendMessage(message.jid,filter.dataValues.text, MessageType.text, {quoted: message.data});
             }
-        }
 
 }))
-Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
-    if(Config.BGMFILTER){
-    let banned = jid.find( Jid => Jid === message.jid);
-    if(banned !== undefined) return
-    if (!!message.mention && message.mention[0] == '918921483992@s.whatsapp.net') {
-await message.client.sendMessage(message.jid, fs.readFileSync('./stickers/mention.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
-    }
-const array = ['Paathu','Mm','sed']
-array.map( async (a) => {
-let pattern = new RegExp(`\\b${a}\\b`, 'g');
-if(pattern.test(message.message)){
-   await message.client.sendMessage(message.jid, fs.readFileSync('./stickers/' + a + '.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted: message.data, ptt: false})
-}
-});
-    }
+
 
 Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
     if(Config.BGMFILTER){
