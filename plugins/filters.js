f7e9bc4589b04d0590f0cb7a665a1ac562d53e6f
saidalisaid2 <86299543+saidalisaid2@users.noreply.github.com>
2021-07-23 22:05:43 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -120,7 +120,7 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
     if (!!message.mention && message.mention[0] == '91860759500@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./stickers/mention.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
     }
-const array = ['Hii','ee','Da','food','Hlo','para','love u','set','sed','jada','Saidali','Hi','sad','don','Loki','nee','bye']
+const array = ['Hii','ee','Da','food','Hlo','para','love u','set','sed','jada','Saidali','Hi','sad','don','Loki','nee','bye','ara','Goodnight','kittilla','Mm','Msd','Njan','nirtheda','nan','parupadi','Pm','poda','podi','song']
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
