c2965add6f3d44560b844058cefefaf755e9f40b
Ameer Suhail <86001869+ameer-kallumthodi@users.noreply.github.com>
2021-07-24 17:07:51 +0530

    Add files via upload

---
+++ plugins/lwolf.js
@@ -0,0 +1,38 @@
+/* # Exclusively from RAVANA 
+# script original owner gos to @lasiya99X 
+# Copyright 2021 All rights reserved 
+*/
+
+const Asena = require('../events');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const Config = require('../config');
+const need = "*type some word after command*\n*command  ENTHEKILUM EZHUTH NENBA"
+
+if (Config.WORKTYPE == 'private') {
+
+    Asena.addCommand({ pattern: 'lwolf ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.zeks.xyz/api/wolflogo?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=LiyaMol&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by Pikachu' })
+
+    }));
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({ pattern: 'lwolf ?(.*)', fromMe: false,dontAddCommandList: true }, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.zeks.xyz/api/wolflogo?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=LiyaMol&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by Pikachu' })
+
+    }));
+    
+}
