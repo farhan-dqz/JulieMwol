41d085acdef979b82c0ceef28ed770791f945b6d
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-07-26 14:18:43 +0530

    Update multiuse.js

--- plugins/multiuse.js
+++ plugins/multiuse.js
@@ -5,7 +5,7 @@ const IG_DESC = "Downloads Image/Video From Instagram"
 
 Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, desc: IG_DESC }, async (message, match) => {
     //if(match[1] == '') return
-    let { data, type } = await instaGram(match[1], 'f3eaf19231f6201c');
+    let { data, type } = await instaGram(match[1], 'd90a9e986e18778b');
     //if(type == undefined) return 
     if (type === 'image') { await message.sendMessage(data, MessageType.image, { caption: "Made By JulieMwol" }) }
     else if (type === 'video') { await message.sendMessage(data, MessageType.video, { caption: "Made By JulieMwol" }) }
