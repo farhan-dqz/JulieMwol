fcedaafc441e92bb856e1eede226018809055353
saidalisaid2 <86299543+saidalisaid2@users.noreply.github.com>
2021-07-27 18:13:36 +0530

    Update bad.js

--- plugins/bad.js
+++ plugins/bad.js
@@ -61,7 +61,7 @@ Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (mess
         let regex4 = new RegExp('poor')
         let regex5 = new RegExp('https://chat.whatsapp.com/')
         let regex6 = new RegExp('andi')
-        let regex7 = new RegExp('myr')
+        let regex7 = new RegExp('+91')
         let regex8 = new RegExp('umban')
         let regex9 = new RegExp('oombi')
         let regex10 = new RegExp('myran')
