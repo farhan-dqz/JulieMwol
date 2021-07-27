3b1afb1d3d45713fcc1e6058dbe23abdb1c6e5b0
saidalisaid2 <86299543+saidalisaid2@users.noreply.github.com>
2021-07-27 13:01:07 +0530

    Update therivar.js

--- plugins/therivar.js
+++ plugins/therivar.js
@@ -41,7 +41,7 @@ let baseURI = '/apps/' + config.HEROKU.APP_NAME;
         THERI_off = 'bad wrd option turned off'
     }
    
-    Asena.addCommand({pattern: 'theri ?(.*)', fromMe: true, desc: l_dsc, usage: '.link no / yes' }, (async (message, match) => {
+    Asena.addCommand({pattern: 'link ?(.*)', fromMe: true, desc: l_dsc, usage: '.link no / yes' }, (async (message, match) => {
         if (match[1] == 'yes') {
                 await heroku.patch(baseURI + '/config-vars', { 
                     body: { 
