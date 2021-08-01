019d77ae654ffdd3838213820c40c039f078b114
Akashakash3 <86599331+Akashakash3@users.noreply.github.com>
2021-08-01 23:04:18 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -895,7 +895,7 @@ else if (config.WORKTYPE == 'public') {
             return;
     
         let 
-            LANG = 'hi',
+            LANG = 'ml',
             ttsMessage = match[1],
             SPEED = 1.0
 
