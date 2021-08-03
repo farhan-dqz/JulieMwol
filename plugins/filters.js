122bcf5b043e2cdd8ce99d9c4808651ba56477a6
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-08-03 18:00:57 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -112,6 +112,12 @@ Asena.addCommand({pattern: 'stop ?(.*)', fromMe: true, desc: Lang.STOP_DESC, don
     }
 }));
 Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
+    
+     if (message.jid === '919383400679-1627924673@g.us') {
+
+            return;
+        } 
+    
         if(Config.BGMFILTER){
         let banned = jid.find( Jid => Jid === message.jid);
         if(banned !== undefined) return
@@ -139,6 +145,12 @@ if(pattern.test(message.message)){
     );
 }));
 Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
+    
+     if (message.jid === '919383400679-1627924673@g.us') {
+
+            return;
+        } 
+    
     if(Config.AUTOSTICKER){
     let banned = jid.find( Jid => Jid === message.jid);
     if(banned !== undefined) return
