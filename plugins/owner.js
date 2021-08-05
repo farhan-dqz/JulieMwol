b2aa42918c33d97ccfc44ddf5abb299e72a4cd7e
booldofss <87748524+booldofss@users.noreply.github.com>
2021-08-05 16:16:07 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'owner', fromMe: false, desc: Lang.WP}, (async (messa
     
     
    
-  r_text[0] = "https://ibb.co/wsp1HvQ";
+  r_text[0] = "https://i.imgur.com/MZDCN2M.jpeg";
     
     
     var i = Math.floor(1*Math.random())
