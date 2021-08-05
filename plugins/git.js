e0fb57014be65ea4f70196d564a80362b4b6e72b
booldofss <87748524+booldofss@users.noreply.github.com>
2021-08-05 16:15:11 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://ibb.co/wsp1HvQ";
+  r_text[0] = "https://i.imgur.com/MZDCN2M.jpeg";
     
     
     var i = Math.floor(1*Math.random())
