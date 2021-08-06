9ae59c0c1f85867d27e56aac12f4edc4894611ae
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-08-06 15:34:52 +0530

    Update fakeid.js

--- plugins/fakeid.js
+++ plugins/fakeid.js
@@ -11,6 +11,7 @@ const got = require('got');
 const Language = require('../language');
 const Lang = Language.getString('weather');
 
+else if (Config.WORKTYPE == 'private') {
 
 Asena.addCommand({pattern: 'fake id ?(.*)', fromMe: true, desc: '' , dontAddCommandList: true }, async (message, match) => {
 	const url = `https://docs-jojo.herokuapp.com/api/fake_identity?lang=en&type=json`;
