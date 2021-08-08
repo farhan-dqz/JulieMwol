d716b98b9f111cf1a7a97dda9b9afa898af18aa6
booldofss <87748524+booldofss@users.noreply.github.com>
2021-08-08 11:55:33 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -1,26 +1,36 @@
 const Asena = require('../events');
+
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+
 const axios = require('axios');
 
 const Language = require('../language');
+
 const Lang = Language.getString('wallpaper');
 
-Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+Asena.addCommand({pattern: 'owner', fromMe: false, desc: Lang.WP}, (async (message, match) => {
 
     var r_text = new Array ();
+
     
+
     
+
    
+
   r_text[0] = "https://i.imgur.com/MZDCN2M.jpeg";
+
     
+
     
+
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater ✦҈͜͡➳✪ɪͥᴛͭsᷤ ᴍͫᴇͤ☚⃟፝✪͜͡S͛aͣcͨhͪuͧs͛aͣmͫuͧ   cͨrͬeͤaͣᴛⷮiͥoͦn👹★✪ৡৢ͜͡➛*
 
-*bgm 🧚‍♀️updation akku💘*
+*bgm 🧚‍♀️updation akku👻*
 
 *owner number wa.me/919744474996*
 
@@ -33,3 +43,27 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
 `}) 
 
 }));
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
