ca1d28665b183d9ec476a7b7d3ac407b49c1eb65
booldofss <87748524+booldofss@users.noreply.github.com>
2021-08-08 22:51:47 +0530

    Update liya.js

--- plugins/liya.js
+++ plugins/liya.js
@@ -18,7 +18,7 @@ const lol = "need some word\n🗡🗡enthelum ezhuthu nenba"
 
         var ttinullimage = await axios.get(`https://docs-jojo.herokuapp.com/api/ttlogo?text1=${encodeURIComponent(match[1])}&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by liyamol*' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by ss-creation-*' })
 
     }));
 }
