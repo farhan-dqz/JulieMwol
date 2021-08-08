ee13edf87379090eacc7962d6ce67bd0de4c4cf8
booldofss <87748524+booldofss@users.noreply.github.com>
2021-08-08 22:05:20 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -117,10 +117,10 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
     if(Config.BGMFILTER){
     let banned = jid.find( Jid => Jid === message.jid);
     if(banned !== undefined) return
-    if (!!message.mention && message.mention[0] == '91860759500@s.whatsapp.net') {
+    if (!!message.mention && message.mention[0] == '919744474996@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./stickers/mention.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
     }
-const array = ['Hii','ee','Da','food','ok','Hlo','sachusamu','para','love u','set','sed','jada','Saidali','Hi','sad','don','Loki','nee','bye','ara','Goodnight','kittilla','Mm','Msd','Njan','nirtheda','nan','parupadi','pm','poda','podi','song','va','Aysheri','ha','set','Comady','ne','Uploding','mathi','e','Ponu','Hlo','Ara','vazha','Eda','ss creation','love','Hy']
+const array = ['Hii','ee','Da','food','njan','halo','nanbaa','ok','Hlo','sachusamu','para','love u','set','sed','jada','Saidali','Hi','sad','don','Loki','nee','bye','ara','Goodnight','kittilla','Mm','Msd','Njan','nirtheda','nan','parupadi','pm','poda','podi','song','va','Aysheri','ha','set','Comady','ne','Uploding','mathi','e','Ponu','Hlo','Ara','vazha','Eda','ss creation','love','Hy']
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
