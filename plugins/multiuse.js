d32c9ff08918d78de986ec6bed4d989097858c27
booldofss <87748524+booldofss@users.noreply.github.com>
2021-08-09 22:21:24 +0530

    Update multiuse.js

--- plugins/multiuse.js
+++ plugins/multiuse.js
@@ -7,8 +7,8 @@ Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, desc: IG_DESC }, async
     //if(match[1] == '') return
     let { data, type } = await instaGram(match[1], '958b7c4b9c34d718');
     //if(type == undefined) return 
-    if (type === 'image') { await message.sendMessage(data, MessageType.image, { caption: "Made By BOT" }) }
-    else if (type === 'video') { await message.sendMessage(data, MessageType.video, { caption: "Made By BOT" }) }
+    if (type === 'image') { await message.sendMessage(data, MessageType.image, { caption: "Made By SACHUSAMU " }) }
+    else if (type === 'video') { await message.sendMessage(data, MessageType.video, { caption: "Made By SACHUSAMU " }) }
 });
 //const axios = require('axios')
 async function instaGram(url, key){
