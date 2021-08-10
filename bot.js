c94fbcc34277dcba490b29080554e0dba9ee56c6
booldofss <87748524+booldofss@users.noreply.github.com>
2021-08-10 14:43:33 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -396,7 +396,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
                 if (config.FULLEVA == 'true') {
                     await conn.sendMessage(conn.user.jid, EVA_ACTİON, MessageType.text)
                 } else {
-                    await conn.sendMessage(conn.user.jid, '\n*ss-creation- Working as Public!🧚‍♀️*\n', MessageType.text);
+                    await conn.sendMessage(conn.user.jid, '\n*ss-creation- Working as Public!😎*\n', MessageType.text);
                 }               
                 await git.fetch();
                 var commits = await git.log([config.BRANCH + '..origin/' + config.BRANCH]);
