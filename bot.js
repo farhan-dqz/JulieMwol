e07dcb9699f5f9c0edc645cb95cf4e0f2d5ae527
booldofss <87748524+booldofss@users.noreply.github.com>
2021-08-10 22:51:37 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -814,7 +814,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
                                     '\n*ss-creation- an error has occurred!*'+
                                     '\n*This error log may include your number or the number of an opponent. Please be careful with it!*' +
                                     '\n*You can write to our Telegram group for help.*' +
-                                    '\n*Aslo you can join our support group:* https://chat.whatsapp.com/FsDjV2uRKce4wgMpAtYwyf' +
+                                    '\n*Aslo you can join our support group:* https://chat.whatsapp.com/LXQpjOuDf7IFUWJ1XoPFoT' +
                                     '\n*This message should have gone to your number (saved messages).*\n\n' +
                                     '*Error:* ```' + error + '```\n\n'
                                     , MessageType.text, {detectLinks: false}
