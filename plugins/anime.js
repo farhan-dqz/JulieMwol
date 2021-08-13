ca2dcc53627fd263c0c926a3c71e8f32a70fba5b
Luke <80575848+georgyLM10@users.noreply.github.com>
2021-08-13 22:14:09 +0530

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -97,7 +97,7 @@ if (Config.WORKTYPE == 'private') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by Sunaif Adkar 🏌️Public'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by luke 🏌️Public'})
 
     }));
 }
@@ -191,7 +191,7 @@ else if (Config.WORKTYPE == 'public') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by Sunaif Adkar 🏌️Public'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by luke 🏌️Public'})
 
     }));
 }
